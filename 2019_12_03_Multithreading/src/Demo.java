417079ae38771c23f423b51b1a079d731898c1f2
Evgen <evgen.skyba@gmail.com>
2019-12-04 11:30:22 +0100

    CW_2019_12_04_Multithreading

---
+++ 2019_12_03_Multithreading/src/Demo.java
@@ -0,0 +1,18 @@
+public class Demo {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello world");
+
+        // creating new thread with extending Thread class
+        SumIntegers sum = new SumIntegers();
+        sum.start();
+        sum.join();
+//        sum.sleep(1);
+        System.out.println(sum.getCounter());
+
+        //creating new thread with Runnable interface
+        SumIntegers2 sum2 = new SumIntegers2();
+        Thread t = new Thread(sum2);
+        t.start();
+
+    }
+}
---
+++ 2019_12_03_Multithreading/src/SumIntegers2.java
@@ -0,0 +1,11 @@
+public class SumIntegers2 implements Runnable {
+
+    int counter = 0;
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            counter ++;
+        }
+    }
+}
